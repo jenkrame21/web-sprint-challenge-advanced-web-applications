fedb0823d8b76ece52f405ef3ce8cd765432090a
Jennifer Kramer <jenkrame@gmail.com>
2021-01-29 14:34:45 -0500

    Still testing...

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,28 +1,77 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { BubblePage as mockFetchBubbles } from "./BubblePage";
-jest.mock('./../api/fetchBubbles');
+import BubblePage from "./BubblePage";
+
+const bubblesColorData = [
+  { 
+    color: "aliceblue",
+    code: { hex: "#f0f8ff" },
+    id: 1
+  },
+  { 
+    color: "limegreen",
+    code: { hex: "#99ddbc" },
+    id: 2
+  },
+  { 
+    color: "aqua",
+    code: { hex: "#00ffff" },
+    id: 3
+  },
+  { 
+    color: "aquamarine",
+    code: { hex: "#7fffd4" },
+    id: 4
+  },
+  { 
+    color: "lilac",
+    code: { hex: "#9a99dd" },
+    id: 5
+  },
+  { 
+    color: "softpink",
+    code: { hex: "#dd99ba" },
+    id: 6
+  },
+  { 
+    color: "bisque",
+    code: { hex: "#dd9a99" },
+    id: 7
+  },
+  { 
+    color: "softyellow",
+    code: { hex: "#dcdd99" },
+    id: 8
+  },
+  { 
+    color: "blanchedalmond",
+    code: { hex: "#ffebcd" },
+    id: 9
+  },
+  { 
+    color: "blue",
+    code: { hex: "#6093ca" },
+    id: 10
+  },
+  { 
+    color: "blueviolet",
+    code: { hex: "#8a2be2" },
+    id: 11
+  }
+];
 
 test("Renders BubblePage without errors", () => {
   // Finish this test
-  render(<mockFetchBubbles />);
+  render(<BubblePage />);
 });
 
 test("Fetches data and renders the bubbles on mounting", () => {
   // Finish this test
-  mockFetchBubbles.mockResolvedValueOnce({
-    data: [
-      {color: "Red", code: { hex: "#FF0000" }, id: 20},
-      {color: "Yellow", code: { hex: "#FFFF00" }, id: 25}
-    ]
-  });
+  render(<BubblePage bubblesColor={bubblesColorData} />);
 
-  render(<mockFetchBubbles />);
-
-  const deleteColorButton = screen.queryByText(/x/i);
-  console.log(deleteColorButton);
+  const searchColor = screen.getByText(/aliceblue/i);
 });
 
 //Task List
-//[] - 1. Setup test for basic rendering of component
+//[x] - 1. Setup test for basic rendering of component
 //[] - 2. Setup test for initial rendering of bubbles on loading
