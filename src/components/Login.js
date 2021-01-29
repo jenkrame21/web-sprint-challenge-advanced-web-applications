14922ccbb3b1a063ea327fc6d0bf44bfece68d3c
Jennifer Kramer <jenkrame@gmail.com>
2021-01-29 11:25:25 -0500

    Created login fields with handleChange. Working on onSubmit function.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Login = () => {
+  const loginCredentials = {
+    username: "",
+    password: ""
+  }
+
+  const [login, setLogin] = useState(loginCredentials);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setLogin({
+      ...login,
+      [name]: value
+    });
+  };
+
+  const onSubmit = () => {
+
+  };
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  
 
   return (
     <>
-      <h1>
-        Welcome to the Bubble App!
-        <p>Build a login page here</p>
-      </h1>
+      <h1>Welcome to the Bubble App!</h1>
+      <form onSubmit={onSubmit}>
+        <label>Username:</label>
+        <input
+          id="username"
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={login.username}
+          onChange={handleChange}
+        />
+        <label>Password:</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={login.password}
+          onChange={handleChange}
+        />
+        <button>Login</button>
+      </form>
     </>
   );
 };
