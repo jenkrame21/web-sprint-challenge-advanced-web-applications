56d7d2df1441595807b920ce2b64990aa5a610db
Jennifer Kramer <jenkrame@gmail.com>
2021-01-29 12:19:37 -0500

    Working on BubblePage component. Get request coming up as 404.

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axiosWithAuth from './../helpers/axiosWithAuth';
 import axios from "axios";
 
 import Bubbles from "./Bubbles";
@@ -6,6 +7,18 @@ import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/colors')
+        .then((res) => {
+          console.log("Success Get BubblesPage:", res);
+          // setColorList(res.data);
+        })
+        .catch((err) => {
+          console.log("Error Get BubblesPage:", err.message);
+        })
+  }, [])
 
   return (
     <>
@@ -18,4 +31,4 @@ const BubblePage = () => {
 export default BubblePage;
 
 //Task List:
-//1. Make an axios call to retrieve all color data and push to state on mounting.
+// [] - 1. Make an axios call to retrieve all color data and push to state on mounting.
