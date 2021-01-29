6184f46e3afacfdce084d60d910efe9ee780842d
Jennifer Kramer <jenkrame@gmail.com>
2021-01-29 11:50:48 -0500

    Got onSubmit to work with axios. Will fix axiosWithAuth later.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import axiosWithAuth from '../helpers/axiosWithAuth';
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   const loginCredentials = {
@@ -8,6 +10,7 @@ const Login = () => {
   }
 
   const [login, setLogin] = useState(loginCredentials);
+  const { push } = useHistory();
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -16,14 +19,24 @@ const Login = () => {
       [name]: value
     });
   };
-
-  const onSubmit = () => {
-
-  };
-
+  
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  
+  const onSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', login)
+      .then((res) => {
+        // Server receives data
+        // console.log("Success Post Login:", res.data.payload);
+        // Client receives data
+        localStorage.setItem('token', res.data.payload);
+        push('/protected');
+      })
+      .catch((err) => {
+        console.log("Error Post Login:", err.message);
+      });
+  };
 
   return (
     <>
@@ -56,8 +69,8 @@ const Login = () => {
 export default Login;
 
 //Task List:
-//1. Build a form containing a username and password field.
-//2. Add whatever state nessiary for form functioning.
-//3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEST "username" and "password" RESPECTIVELY.
-//4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
-//5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
+//[x] - 1. Build a form containing a username and password field.
+//[x] - 2. Add whatever state necessary for form functioning.
+//[x] - 3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT "username" and "password" RESPECTIVELY.
+//[x] - 4. If either the username or password is not displayed EXACTLY the following words: Username or Password not valid.
+//[x] - 5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
